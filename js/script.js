503a701725a552617777520a90fdca1e12d661cc
cattandrej <45920569+cattandrej@users.noreply.github.com>
2020-06-22 17:37:31 +0200

    better card margin

--- js/script.js
+++ js/script.js
@@ -1,40 +1,66 @@
 var cardsAmount = $(".card").length;
-var cardContainerWidth = $(".cards").width();
-var cardWidth = $(".card").width() + 2;
-var remainingSpace = ((cardContainerWidth / cardWidth) % 1) * cardWidth;
-var cardsPerRow = (cardContainerWidth / cardWidth) - ((cardContainerWidth / cardWidth) % 1);
-var cardMargin = remainingSpace / (cardsPerRow - 1);
-
-if (cardMargin < 16) {
-    remainingSpace += cardWidth;
-    cardsPerRow--;
-    cardMargin = remainingSpace / (cardsPerRow - 1);
-}
-
-console.log("cardsAmount " + cardsAmount);
-console.log("cardContainerWidth " + cardContainerWidth);
-console.log("cardWidth " + cardWidth);
-console.log("remainingSpace " + remainingSpace);
-console.log("cardsPerRow " + cardsPerRow);
-console.log("cardMargin " + cardMargin);
+var cardContainerWidth = 0;
+var cardWidth = 0;
+var remainingSpace = 0;
+var cardsPerRow = 0;
+var cardMargin = 0;
 
 
-$(".card").each(function(index) {
-    if ((index + 1) % (cardsPerRow) != 0) {
-        $(this).css("margin-right", cardMargin + "px");
-    }
+updateCardMargins(cardsAmount);
+$( window ).resize(function() {
+    updateCardMargins(cardsAmount);
 });
 
-
 function updateCardsVisibility(id) {
+    var cardCont = 0;
     console.log("click on checkbox " + id);
 
     $("#" + id).toggleClass("checked");
 
-    $(".card").each(function() {
+    $(".card").each(function () {
         if ($(this).hasClass(id)) {
             $(this).toggleClass("hiddenCard");
         }
+        if (!($(this).hasClass("hiddenCard"))) {
+            cardCont++;
+        } 
     });
 
+    updateCardMargins(cardCont);
+}
+
+function updateCardMargins(n) {
+    cardsAmount = n;
+    cardContainerWidth = $(".cards").width();
+    cardWidth = 225 + 2;
+    remainingSpace = ((cardContainerWidth / cardWidth) % 1) * cardWidth;
+    cardsPerRow = (cardContainerWidth / cardWidth) - ((cardContainerWidth / cardWidth) % 1);
+    cardMargin = remainingSpace / (cardsPerRow - 1);
+
+    if (cardMargin < 0) {
+        remainingSpace += cardWidth;
+        cardsPerRow--;
+        cardMargin = remainingSpace / (cardsPerRow - 1);
+    }
+
+    var cont = 0;
+    $(".card").each(function (index) {
+        $(this).css("margin-right", "0px");
+        if (!($(this).hasClass("hiddenCard"))) {
+            if ((cont + 1) % (cardsPerRow) != 0) {
+                $(this).css("margin-right", cardMargin + "px");
+            }
+        } else {
+            cont--;
+        }
+        cont++;
+    });   
+
+    console.log("cardsAmount: \t" + cardsAmount);
+    console.log("cardContainerWidth: \t" + cardContainerWidth);
+    console.log("cardWidth: \t" + cardWidth);
+    console.log("remainingSpace: \t" + remainingSpace);
+    console.log("cardsPerRow: \t" + cardsPerRow);
+    console.log("cardMargin: \t" + cardMargin);
+    console.log(" *** ");
 }
