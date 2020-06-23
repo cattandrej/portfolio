43f216e67db98abaf62f5606ad46ec1a97f1e4c3
cattandrej <45920569+cattandrej@users.noreply.github.com>
2020-06-23 11:14:39 +0200

    better filtering

--- js/script.js
+++ js/script.js
@@ -1,3 +1,27 @@
+//
+// Filter tags
+//
+// tags = [ id ] [ enabled/disabled ]
+var tags = [
+];
+
+$(".checkbox").each(function (index) {
+
+    var temp = [
+        "id_not_initialized",
+        true
+    ];
+
+    console.log($(this).attr("id"));
+    temp[0] = $(this).attr("id");
+
+    tags.push(temp);
+});
+
+
+//
+// Card amount
+//
 var cardsAmount = $(".card").length;
 var cardContainerWidth = 0;
 var cardWidth = 0;
@@ -5,29 +29,10 @@ var remainingSpace = 0;
 var cardsPerRow = 0;
 var cardMargin = 0;
 
-
 updateCardMargins(cardsAmount);
-$( window ).resize(function() {
+$(window).resize(function () {
     updateCardMargins(cardsAmount);
 });
-
-function updateCardsVisibility(id) {
-    var cardCont = 0;
-    console.log("click on checkbox " + id);
-
-    $("#" + id).toggleClass("checked");
-
-    $(".card").each(function () {
-        if ($(this).hasClass(id)) {
-            $(this).toggleClass("hiddenCard");
-        }
-        if (!($(this).hasClass("hiddenCard"))) {
-            cardCont++;
-        } 
-    });
-
-    updateCardMargins(cardCont);
-}
 
 function updateCardMargins(n) {
     cardsAmount = n;
@@ -54,13 +59,79 @@ function updateCardMargins(n) {
             cont--;
         }
         cont++;
-    });   
+    });
 
-    console.log("cardsAmount: \t" + cardsAmount);
-    console.log("cardContainerWidth: \t" + cardContainerWidth);
-    console.log("cardWidth: \t" + cardWidth);
-    console.log("remainingSpace: \t" + remainingSpace);
-    console.log("cardsPerRow: \t" + cardsPerRow);
-    console.log("cardMargin: \t" + cardMargin);
-    console.log(" *** ");
+    // console.log("cardsAmount: \t" + cardsAmount);
+    // console.log("cardContainerWidth: \t" + cardContainerWidth);
+    // console.log("cardWidth: \t" + cardWidth);
+    // console.log("remainingSpace: \t" + remainingSpace);
+    // console.log("cardsPerRow: \t" + cardsPerRow);
+    // console.log("cardMargin: \t" + cardMargin);
+    // console.log(" *** ");
+}
+
+//
+// Card visibility
+//
+function updateCardsVisibility(id) {
+    var cardCont = 0;
+    console.log("click on checkbox " + id);
+
+    for (var i = 0; i < tags.length; i++) {
+        if (tags[i][0] === id) {
+            tags[i][1] = !tags[i][1];
+        }
+    }
+
+    $("#" + id).toggleClass("checked");
+
+    // $(".card").each(function () {
+    //     if ($(this).hasClass(id)) {
+    //         $(this).toggleClass("hiddenCard");
+    //     }
+    //     if (!($(this).hasClass("hiddenCard"))) {
+    //         cardCont++;
+    //     } 
+    // });
+
+    $(".card-tags > p").each(function () {
+        console.log(id + "; " + ($(this).attr("class")));
+        if ($(this).hasClass(id)) {
+            $(this).toggleClass("tag-enabled");
+        }
+    });
+
+    $(".card").each(function () {
+
+        var removedClasses = 0;
+        $($(this)).find(".card-tags").children().each(function () {
+            for (var i = 0; i < tags.length; i++) {
+                if ($(this).hasClass(tags[i][0])) {
+                    if (tags[i][1]) {
+                        $(this).addClass("tag-enabled");
+                    } else {
+                        $(this).removeClass("tag-enabled");
+                        removedClasses++;
+                    }
+                }
+            }
+        });
+
+        if (removedClasses == $($(this)).find(".card-tags").children().length) {
+            $(this).addClass("hiddenCard");
+        } else {
+            $(this).removeClass("hiddenCard");
+        }
+
+        console.log("removed classes: " + removedClasses + "\nall classes: " + $(this).children().length);
+        for (var tmp = 0; tmp < $($(this)).find(".card-tags").children().length; tmp++) {
+            $($(this)).find(".card-tags").children().each( function () {
+                console.log(tmp + ": " + $(this).attr("class"));
+            });
+        }
+        console.log("***");
+    });
+
+
+    updateCardMargins(cardCont);
 }
