b50ce1d7313a7289d65edfba64ccbc2a0f155438
cattandrej <45920569+cattandrej@users.noreply.github.com>
2020-06-25 10:58:12 +0200

    add gallery

---
+++ js/photogallery.js
@@ -0,0 +1,64 @@
+var imgElements = [
+];
+var currentImagePos = 0;
+
+$("img").each(function (i) {
+    var imgElement = [
+        i, $(this).attr("src")
+    ]
+    imgElements.push(imgElement);
+});
+imgElements.shift();
+
+$('img').click(function(){
+    console.log("click on " + $(this).attr("src"));
+    openPhotogallery(imgElements, $(this).attr("src"));       
+ });
+
+ function openPhotogallery(imgList, currentImage) {
+     console.log("photogallery opened");
+     var index = 0;
+     while (currentImage !== imgList[index][1]) {
+         index++;
+     }
+     currentImagePos = index;
+     $(".photogallery img").attr("src", imgList[currentImagePos][1]);
+     $(".photogallery").removeClass("hide");
+ }
+
+ function closeGallery() {
+    $(".photogallery").addClass("hide");
+ }  
+
+ $(document).keyup(function(e) {
+    if (e.key === "Escape") { // escape key maps to keycode `27`
+    $(".photogallery").addClass("hide");
+   }
+});
+
+// $(".photogallery-img").swiperight(function() {
+//     nextImg();
+// });
+
+// $(".photogallery-img").swipeleft(function() {
+//     prevImg();
+// });
+
+function prevImg() {
+    if (currentImagePos > 0) {
+        currentImagePos--;
+    } else {
+        currentImagePos = imgElements.length - 1;
+    }
+    $(".photogallery img").attr("src", imgElements[currentImagePos][1]);
+}  
+
+ function nextImg() {
+     console.log(imgElements);
+    if (currentImagePos < imgElements.length -1) {
+        currentImagePos++;
+    } else {
+        currentImagePos = 0;
+    }
+    $(".photogallery img").attr("src", imgElements[currentImagePos][1]);
+ }  
